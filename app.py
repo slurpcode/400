0e6ce457428bdf953f1bd5e59bfa9e65dbe885cf
John Bampton <jbampton@users.noreply.github.com>
2017-09-28 03:22:59 +1000

    Add docstrings to all 3 functions.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """Homepage with the GitHub Leaderboard."""
     arr = []
     with open(site_path + 'data/task.csv') as fname:
         for line in fname:
@@ -15,12 +16,14 @@ def home():
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """Jedi style 404 Not Found page."""
     return render_template('page_not_found.html'), 404
 
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
+    """Hello world Flask style."""
     return render_template('hello.html', name=name)
 
 
