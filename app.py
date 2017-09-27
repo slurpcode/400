b6021702c815c28229ef690e364f43344f305599
John Bampton <jbampton@users.noreply.github.com>
2017-09-28 03:18:37 +1000

    Add docstring. Indent template.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# A very simple Flask Hello World app for you to get started with...
+"""A very simple Flask Hello World app for you to get started with..."""
 from flask import Flask, render_template
 
 app = Flask(__name__)
